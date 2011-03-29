5f091e91473ed15ea56783a5fe46a7f7e803d176
ricko <ricko@localhost.localdomain>
2011-03-29 17:03:24 +0700

    bikin adminn

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Kategori(models.Model):
+    nama = models.CharField(max_length=255)
+    deskripsi = models.CharField(max_length=255)
+    
+    def __unicode__(self):
+        return self.nama
+
+class Post(models.Model):
+    
+    judul = models.CharField(max_length=255)
+    isi = models.TextField()
+    tgl_buat = models.DateTimeField(auto_now_add=True)
+    kategori = models.ForeignKey('Kategori')
+
+    def __unicode__(self):
+        return self.judul
+        
+class Tamu(models.Model):
+    nama = models.CharField(max_length=255)
+    email = models.EmailField()
+    web = models.CharField(max_length=255)
+    telp = models.CharField(max_length=255)
+    tgl_daftar = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return self.nama
