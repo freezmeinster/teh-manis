1a413a977a6a6acae25587be20593924f36489e8
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2011-04-04 10:52:26 +0700

    nambahin paging

--- apps/kotakuang/views.py
+++ apps/kotakuang/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.shortcuts import render_to_response 
 from django.template.defaultfilters import slugify
 from django.template import RequestContext
@@ -6,58 +7,74 @@ from kotakuang.models import Kategori,Pengeluaran,Pemasukan
 
 def home(request):
     return render_to_response('kotakuang/index.html',{
-	'url' : 'home',
-	},context_instance=RequestContext(request))
+    'url' : 'home',
+    },context_instance=RequestContext(request))
 
 def kategori(request):
     kategori = Kategori.objects.filter(user=request.user)
-    
     if request.method == 'POST' :
-	data = request.POST.copy()
-	data['user'] = request.user.id
-	data['slug'] = slugify(data['nama'])
-	form = KategoriForm(data)
-	if form.is_valid():
-	    form.save()
+        data = request.POST.copy()
+        data['user'] = request.user.id
+        data['slug'] = slugify(data['nama'])
+        form = KategoriForm(data)
+        if form.is_valid():
+            form.save()
     
     return render_to_response('kotakuang/kategori.html',{
-	'url' : 'kategori',
-	'kategori' : kategori,
-	'form' : KategoriForm(),
-	},context_instance=RequestContext(request))
+    'url' : 'kategori',
+    'kategori' : kategori,
+    'form' : KategoriForm(),
+    },context_instance=RequestContext(request))
 
 def pemasukan(request):
-	pemasukan = Pemasukan.objects.filter(user=request.user)
-	if request.method == 'POST' :
-		data = request.POST.copy()
-		data['user'] = request.user.id
-		form  = PemasukanForm(data)
-		if form.is_valid():
-			form.save()
-			
-	return render_to_response('kotakuang/pemasukan.html',{
-		'url' : 'pemasukan',
-		'pemasukan' : pemasukan,
-		'form' : PemasukanForm(),
-	},context_instance=RequestContext(request))
-	
+    pemasukan = Pemasukan.objects.filter(user=request.user)
+    paginator = Paginator(pemasukan, 6)
+    page = int(request.GET.get('page', '1'))
+    # If page request (9999) is out of range, deliver last page of results.
+    try:
+        post_page = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        post_page = paginator.page(paginator.num_pages)
+    
+    if request.method == 'POST' :
+        data = request.POST.copy()
+        data['user'] = request.user.id
+        form  = PemasukanForm(data)
+        if form.is_valid():
+            form.save()
+            
+    return render_to_response('kotakuang/pemasukan.html',{
+        'url' : 'pemasukan',
+        'pemasukan' : post_page,
+        'form' : PemasukanForm(),
+    },context_instance=RequestContext(request))
+    
 def pengeluaran(request):
-	pengeluaran = Pengeluaran.objects.filter(user=request.user)
-	if request.method == 'POST' :
-	    data = request.POST.copy()
-	    data['user'] = request.user.id
-	    data['total'] = int(data['jumlah']) * int(data['harga'])
-	    form = PengeluaranForm(data)
-	    if form.is_valid():
-		form.save()
-	
-	return render_to_response('kotakuang/pengeluaran.html',{
-	    'url' : 'pengeluaran',
-	    'pengeluaran' : pengeluaran,
-	    'form' : PengeluaranForm,
-	},context_instance=RequestContext(request))
+    pengeluaran = Pengeluaran.objects.filter(user=request.user)
+    
+    paginator = Paginator(pengeluaran, 6)
+    page = int(request.GET.get('page', '1'))
+    # If page request (9999) is out of range, deliver last page of results.
+    try:
+        post_page = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        post_page = paginator.page(paginator.num_pages)
+    
+    if request.method == 'POST' :
+        data = request.POST.copy()
+        data['user'] = request.user.id
+        data['total'] = int(data['jumlah']) * int(data['harga'])
+        form = PengeluaranForm(data)
+        if form.is_valid():
+            form.save()
+    
+    return render_to_response('kotakuang/pengeluaran.html',{
+        'url' : 'pengeluaran',
+        'pengeluaran' : post_page,
+        'form' : PengeluaranForm,
+    },context_instance=RequestContext(request))
     
 def about(request):
     return render_to_response('kotakuang/about.html',{
-	'url' : 'about',
-	},context_instance=RequestContext(request))
+    'url' : 'about',
+    },context_instance=RequestContext(request))
--- apps/main/views.py
+++ apps/main/views.py
@@ -4,23 +4,23 @@ from main.forms import UserRegistrationForm
 
 def home(request):
     return render_to_response('main/index.html',{
-	'url' : 'home',
+    'url' : 'home',
     },context_instance=RequestContext(request))
-	
+    
 def register(request,apps_id):
    
     if apps_id == 1 :
-	redirect_url = 'kotakuang_home'
+        redirect_url = 'kotakuang_home'
     else :
-	redirect_url = 'main'
+        redirect_url = 'main'
     
     if request.method == 'POST':
-	form = UserRegistrationForm(request.POST)
-	if form.is_valid():
-	    form.save()
-	    return redirect(redirect_url)
-	    
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(redirect_url)
+        
     return render_to_response('main/register.html',{
-	'form': UserRegistrationForm(),
-	'url' : 'register',
+    'form': UserRegistrationForm(),
+    'url' : 'register',
     },context_instance=RequestContext(request))
